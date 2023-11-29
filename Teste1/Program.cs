9e6f62948ac49526f36ff9836b017838b5ccf881
Matheus Vinicius Cortez Da Cunha Beluqui <m.vinicius@btp.com.br>
2023-11-29 13:00:56 -0300

    Primeiro Commit

---
+++ Teste1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Teste1
+{
+    class Program
+    {
+        //TODO
+        /* 
+         * Desenvolva um método que receba a string "abbcbbb". O retorno desse método deverá ser "abcb", 
+         * logo o próximo caracter não deverá ser igual ao anterior, o método deve ser capaz de tratar qualquer string.
+        */
+
+        static void Main(String[] args)
+        {
+        }
+    }
+}
---
+++ Teste2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Teste2
+{
+    /*
+      * TODO
+      * Implementar CanBeConnect para identificar se o Friend A pode se Conectar com o Friend C. Para isso, é necessário ter um friend em comum.
+         - Caso sim, o método deverá retornar True
+         - Caso não, o método deverá retornar False
+      */
+    class Friend
+    {
+        public string Email { get; private set; }
+
+        public ICollection<Friend> Friends { get; private set; }
+
+        public Friend(string email)
+        {
+            this.Email = email;
+            this.Friends = new List<Friend>();
+        }
+
+        public void AddFriendship(Friend friend)
+        {
+            this.Friends.Add(friend);
+        }
+
+        public bool CanBeConnect(Friend friendC)
+        {
+            return true;
+        }
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
